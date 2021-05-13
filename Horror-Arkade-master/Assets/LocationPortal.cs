3c7466e9d34f5e8bc96d65e755422fc5a9003f14
Gordon Arber <Gordon.arber@gmail.com>
2021-05-13 16:41:36 -0400

    Updated portal

---
+++ Horror-Arkade-master/Assets/LocationPortal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocationPortal : MonoBehaviour
+{
+    
+    [Header("Target")]
+    [SerializeField] private Transform targetDestination;
+    
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject == NewPlayer.Instance.gameObject && NewPlayer.Instance.grounded)
+        {
+            // col.transform.position = targetDestination.transform.position;
+            col.transform.position = new Vector2(targetDestination.position.x, targetDestination.position.y);
+        }
+    }
+}
