7be3ac1254b164259e9fe8856056948c444f9341
Gordon Arber <Gordon.arber@gmail.com>
2021-05-14 22:26:57 -0400

    Implemented Game Event SO System

---
+++ Horror-Arkade-master/Assets/Scripts/GameEventListener.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class GameEventListener : MonoBehaviour
+{
+    [SerializeField] GameEvent gameEvent; 
+    [SerializeField] UnityEvent response; 
+
+    void Awake() => gameEvent.Register(this);
+
+    void OnDestroy() => gameEvent.Unregister(this);
+
+    public void RaiseEvent() => response.Invoke();
+}
