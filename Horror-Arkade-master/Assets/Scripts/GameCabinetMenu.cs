4dc3b954075da55833d9e6daa515d5a7724baf00
Gordon Arber <Gordon.arber@gmail.com>
2021-05-17 17:27:14 -0400

    Arkade Menu System

---
+++ Horror-Arkade-master/Assets/Scripts/GameCabinetMenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameCabinetMenu : MonoBehaviour
+{
+
+   public void CollectTicket()
+   {
+      Debug.Log("Collecting Ticket");
+   }
+   
+   public void ExitCabinet()
+   {
+      Debug.Log("Exiting Cabinet");
+   }
+    
+}
