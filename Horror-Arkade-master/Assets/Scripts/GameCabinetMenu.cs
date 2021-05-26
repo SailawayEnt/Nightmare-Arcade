03bd563ee418c1dc714ab1ec3b36bca23be9541f
Gordon Arber <Gordon.arber@gmail.com>
2021-05-25 22:02:10 -0400

    getting spaceship game to work

--- Horror-Arkade-master/Assets/Scripts/GameCabinetMenu.cs
+++ Horror-Arkade-master/Assets/Scripts/GameCabinetMenu.cs
@@ -3,11 +3,20 @@ using UnityEngine.SceneManagement;
 
 public class GameCabinetMenu : MonoBehaviour
 {
-
+   [Header("Target")] 
+   [SerializeField] Transform targetDestination;
+   
+   [SerializeField] CabinetPlayer cabinetPlayer;
    [SerializeField] private ScenesData _scenesData;
    [SerializeField] private GameEvent onRecievedTicket;
    public ConsumableItem ticketInventory;
 
+   public void StartGame()
+   {
+      var position = targetDestination.position;
+      cabinetPlayer.transform.position = new Vector2(position.x, position.y);
+   }
+   
    public void CollectTicket()
    {
       if (ticketInventory.CurrentStack < ticketInventory.MaxStack)
