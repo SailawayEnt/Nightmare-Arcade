1e74e5f26eb44574cccb5429d43c3acef285a0e8
Gordon Arber <Gordon.arber@gmail.com>
2021-06-03 17:41:54 -0400

    Setting Up Input Settings

---
+++ Horror-Arkade-master/Assets/Scripts/Erik Clean-Up/PlayerController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    // private float Horizontal
+    // {
+    //     get
+    //     {
+    //         var horizontal = 
+    //     }
+    // }
+}
