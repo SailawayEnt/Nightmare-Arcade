ee22efdbe6df9ea1b9679765a2810eb69105e2a3
Gordon Arber <Gordon.arber@gmail.com>
2021-08-24 20:06:45 -0400

    Arkade with Animating Flags

--- Horror-Arkade-master/Assets/Scripts/Other/TriggerDoorController.cs
+++ Horror-Arkade-master/Assets/Scripts/Other/TriggerDoorController.cs
@@ -1,15 +1,23 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+
 
 public class TriggerDoorController : MonoBehaviour
 {
-    [SerializeField] Animator myDoor = null;
+    /*[SerializeField] Animator myDoor = null;
 
     [SerializeField] bool openTrigger = false;
-    [SerializeField] bool closeTrigger = false;
+    [SerializeField] bool closeTrigger = false;*/
 
+    Animator _animator;
+
+    bool _doorOpen;
+
+    void Start()
+    {
+        _doorOpen = false;
+        _animator = GetComponent<Animator>();
+    }
+    /*
     void OnTriggerEnter2D(Collider2D col)
     {
         
@@ -33,5 +41,26 @@ public class TriggerDoorController : MonoBehaviour
                 myDoor.Play("DoorClose", 0, 0.0f);
             }
         }
+    }*/
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject == NewPlayer.Instance.gameObject && !_doorOpen)
+        {
+            _doorOpen = true;
+            DoorControl("Open");
+        }
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (_doorOpen)
+        {
+            _doorOpen = false;
+            DoorControl("Close");
+        }
+    }
+
+    void DoorControl(string direction)
+    {
+        _animator.SetTrigger((direction));
     }
 }
