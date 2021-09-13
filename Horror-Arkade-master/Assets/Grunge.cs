438d8bf9e2b97effc2f1f2653fa5850a2a6921a4
Gordon Arber <Gordon.arber@gmail.com>
2021-09-13 16:29:43 -0400

    grunge wall prefab

---
+++ Horror-Arkade-master/Assets/Grunge.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grunge : MonoBehaviour
+{
+    [SerializeField] GameObject grunge;
+    [SerializeField] bool enableGrunge = true;
+
+    private void Awake()
+    {
+        if (grunge != null)
+        {
+            grunge.SetActive(enableGrunge);
+        }
+    }
+}
