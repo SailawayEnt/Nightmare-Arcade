7d6a3d33302226d6b8fe28a7ac1c90d3f0cb0faf
Gordon Arber <Gordon.arber@gmail.com>
2021-09-24 15:56:34 -0400

    ADD: Bridge prefab, Happy Hotel Sign

---
+++ Horror-Arkade-master/Assets/BridgePicker.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+[ExecuteAlways]
+public class BridgePicker : MonoBehaviour
+{
+
+    enum BridgeStyle
+    {
+        Style1,
+        Style2,
+    }; 
+
+    [SerializeField] BridgeStyle boxStyle;
+
+    [SerializeField] GameObject[] bridges = new GameObject[2];
+
+    void Awake()
+    {
+        ChangeBoxes();
+    }
+
+    void Update()
+    {
+        if (Application.isEditor)
+        {
+            ChangeBoxes();
+        }
+    }
+
+    void ChangeBoxes()
+    {
+        switch (boxStyle)
+        {
+            case BridgeStyle.Style1:
+                bridges[0].SetActive(true);
+                bridges[1].SetActive(false);
+                break;
+            case BridgeStyle.Style2:
+                bridges[0].SetActive(false);
+                bridges[1].SetActive(true);
+                break;
+        }
+    }
+}
